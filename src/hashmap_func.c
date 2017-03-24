8988df30e86fb6be944c9b4792b5c909a9eabc52
akpenou <kardbian@debian>
2017-03-24 01:04:35 +0100

    [UPD] adding bucket growing

--- src/hashmap_func.c
+++ src/hashmap_func.c
@@ -9,7 +9,7 @@ t_hashmap	*hashmap_init(void)
 		return (NULL);
 	hashmap->nb_elements = 0;
 	hashmap->shift = (32 - 8);
-	hashmap->nb_buckets = ((1 << 33) - 1) >> hash->shift;
+	hashmap->nb_buckets = ((1 << 33) - 1) >> hashmap->shift;
 	if (!(hashmap->mem = (t_hashmap *)malloc(sizeof(t_pair) * hashmap->nb_buckets)))
 	{
 		free(hashmap);
@@ -18,15 +18,24 @@ t_hashmap	*hashmap_init(void)
 	return (hashmap);
 }
 
-t_hashmap	*hashmap_add(t_hashmap *hashmap, t_pair *pair)
+t_hashmap	*hashmap_add_elem(t_hashmap *hashmap, t_pair *pair)
 {
+	hashmap->nb_elements++;
+	if ((hashmap->nb_elements >> 2) >= hashmap->nb_buckets && hashmap->shift > 0)
+	{
+		hashmap->shift -= 8;
+		hashmap->nb_buckets = ((1 << 33) - 1) >> hashmap->shift;
+		if (!(hashmap->mem = (t_hashmap *)realloc(sizeof(t_pair) * hashmap->nb_buckets)))
+			return (NULL);
+	}
 	index = superfasthash(pair->key) >> hashmap->shift;
 	hashmap->mem[index] = pair_insert(hashmap->mem[index], pair);
 	return (hashmap);
 }
 
-t_hashmap	*hashmap_del(t_hashmap *hashmap, t_pair *pair)
+t_hashmap	*hashmap_del_elem(t_hashmap *hashmap, t_pair *pair)
 {
+	hashmap->nb_elements--;
 	index = superfasthash(pair->key) >> hashmap->shift;
 	hashmap->mem[index] = pair_del(hashmap->mem[index], pair);
 	return (hashmap);
